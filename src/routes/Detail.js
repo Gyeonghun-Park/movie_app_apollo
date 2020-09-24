a7d98b6be9087e685c049b74cf5c55e2d3b2c886
Gyeonghun Park <gp112795@gmail.com>
2020-09-24 16:19:29 -0400

    Addign Suggestions

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import { useQuery, gql } from "@apollo/client";
 import styled from "styled-components";
+import Movie from "../components/Movie";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -12,22 +13,39 @@ const GET_MOVIE = gql`
       rating
       description_intro
     }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
   }
 `;
 
 const Container = styled.div`
-  height: 100vh;
+  height: 90vh;
   background-image: linear-gradient(-45deg, #d754ab, #fd723a);
   width: 100%;
   display: flex;
   justify-content: space-around;
   align-items: center;
   color: white;
+  margin-bottom: -50px;
 `;
 
 const Column = styled.div`
-  margin-left: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   width: 50%;
+  height: 70%;
+  display: flex;
+`;
+
+const Suggestions = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Title = styled.h1`
@@ -45,12 +63,30 @@ const Description = styled.p`
 `;
 
 const Poster = styled.div`
-  width: 25%;
+  width: 20%;
   height: 60%;
   background-color: transparent;
   background-image: url(${(props) => props.bg});
   background-size: cover;
   background-position: center center;
+`;
+
+const Suggestion = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
+const Test = styled.div`
+  position: relative;
+  top: -80px;
+  text-align: center;
+  font-size: 35px;
+  margin-bottom: 10px;
+  color: white;
 `;
 
 export default () => {
@@ -59,21 +95,26 @@ export default () => {
     variables: { id: parseInt(id) },
   });
   return (
-    <Container>
-      <Column>
-        <Title>{loading ? "Loading..." : data.movie.title}</Title>
-        {!loading && data.movie && (
-          <>
-            <Subtitle>
-              {data.movie.language} · {data.movie.rating}
-            </Subtitle>
-            <Description>{data.movie.description_intro}</Description>
-          </>
-        )}
-      </Column>
-      <Poster
-        bg={data && data.movie ? data.movie.medium_cover_image : ""}
-      ></Poster>
-    </Container>
+    <>
+      <Container>
+        <Column>
+          <Title>{loading ? "Loading..." : data?.movie?.title}</Title>
+          <Subtitle>
+            {data?.movie?.language} · {data?.movie?.rating}
+          </Subtitle>
+          <Description>{data?.movie?.description_intro}</Description>
+        </Column>
+        <Poster bg={data?.movie?.medium_cover_image}></Poster>
+      </Container>
+      <Test>Suggestions relating to {data?.movie?.title}</Test>
+
+      <Suggestions>
+        <Suggestion>
+          {data?.suggestions?.map((s) => (
+            <Movie key={s.id} id={s.id} bg={s.medium_cover_image} />
+          ))}
+        </Suggestion>
+      </Suggestions>
+    </>
   );
 };
